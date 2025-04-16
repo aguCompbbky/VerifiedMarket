f8666ca288ee5abec7e868e2c3ef7d0bed9fedac
aguCompbbky <agumobile25@gmail.com>
2025-04-16 22:44:12 +0300

    use_hosting

--- lib/utils/services/connection.dart
+++ lib/utils/services/connection.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Connection {
-  static const String baseUrl = "http://10.0.2.2/api"; // XAMPP için
+  static const String baseUrl ="http://agumobile.site" ;           //"http://10.0.2.2/api"; // XAMPP için
 
   static String loggedInEmail = "";
 
